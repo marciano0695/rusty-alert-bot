567b2590b523a1009b56f810bb427fe631770e7d
Márcio Nunes <marcio@dynamik.pt>
2023-12-16 19:56:46 +0000

    add: command ping to specify id channel

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+mod commands;
+
 use anyhow::anyhow;
 use serenity::async_trait;
+use serenity::builder::{CreateInteractionResponse, CreateInteractionResponseMessage};
+use serenity::model::application::Interaction;
 use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
+use serenity::model::id::GuildId;
 use serenity::prelude::*;
 use shuttle_secrets::SecretStore;
 use tracing::{error, info};
@@ -10,6 +15,25 @@ struct Bot;
 
 #[async_trait]
 impl EventHandler for Bot {
+    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+        if let Interaction::Command(command) = interaction {
+            println!("Received command interaction: {command:#?}");
+
+            let content = match command.data.name.as_str() {
+                "ping" => Some(commands::ping::run(&command.data.options())),
+                _ => Some("not implemented :(".to_string()),
+            };
+
+            if let Some(content) = content {
+                let data = CreateInteractionResponseMessage::new().content(content);
+                let builder = CreateInteractionResponse::Message(data);
+                if let Err(why) = command.create_response(&ctx.http, builder).await {
+                    println!("Cannot respond to slash command: {why}");
+                }
+            }
+        }
+    }
+
     async fn message(&self, ctx: Context, msg: Message) {
         if msg.content == "!hello" {
             if let Err(e) = msg.channel_id.say(&ctx.http, "world!").await {
@@ -18,8 +42,16 @@ impl EventHandler for Bot {
         }
     }
 
-    async fn ready(&self, _: Context, ready: Ready) {
+    async fn ready(&self, ctx: Context, ready: Ready) {
         info!("{} is connected!", ready.user.name);
+
+        let guild_id = GuildId::new(1185630639597293680);
+
+        let commands = guild_id
+            .set_commands(&ctx.http, vec![commands::ping::register()])
+            .await;
+
+        println!("I now have the following guild slash commands: {commands:#?}");
     }
 }
 
